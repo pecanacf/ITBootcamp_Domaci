986848452f75a6a4ce3e2f305093870cad6bc78f
pecanacf <pecanacf@gmail.com>
2021-06-24 12:52:28 +0200

    kompletiran domaci od petka

---
+++ Nedelja 9/Nedelja/agencija_klasa.php
@@ -0,0 +1,46 @@
+<?php
+
+class TuristickaAgencija{
+    public $conn;
+
+    function __construct($baza){
+        $this->conn = new mysqli('localhost', 'root', '', $baza);
+        if ($this->conn->connect_error)
+            die('Greska: '. $this->conn->connect_error);
+    }
+
+    function izvrsi_select($upit){
+        $podaci = $this->conn->query($upit);
+        if($podaci === false)
+            return ['uspesno'=>false, 'poruka'=>$this->conn->error];
+        else{
+            $niz = $podaci->fetch_all(MYSQLI_ASSOC);//kreira asoc niz
+            return ['uspesno'=>true, 'niz'=>$niz];
+        }
+    }
+    function prikazi_destinacije(){
+        $r = $this->izvrsi_select("SELECT DEST.drzava,DEST.grad,PUT.datum_polaska,PUT.datum_povratka 
+        FROM `putovanja` AS PUT 
+        JOIN destinacije AS DEST ON PUT.id_destinacija=DEST.id");
+        if($r['uspesno'] == true){
+            return $r['niz'];
+        }else{
+            die("Neuspesan upit: ".$r['poruka']);
+        }
+        } 
+        function otkazi_rezervaciju($broj_pasosa){
+            $odg=$this->conn->query("DELETE FROM `spisak_putnika_po_putovanju` WHERE broj_pasosa=$broj_pasosa");
+        if($odg===false){
+            die("nije izvrsen upit: ".$this->conn->error);
+        }else{
+            echo "uspesno izvrsen upit";
+        }
+        }    
+    }
+    $b=new TuristickaAgencija('turisticka_agencija');
+
+  
+
+
+
+?>
